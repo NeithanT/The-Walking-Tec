c86cb0adde247c8d8f04ab76b8c1f9cc4c16b5a7
NeithanT <n.vargas.5@estudiantec.cr>
2025-10-23 23:06:28 -0600

    Working Version of Entity Rows

--- TheWalkingTec/src/main/java/Configuration/EntityRow.java
+++ TheWalkingTec/src/main/java/Configuration/EntityRow.java
@@ -1,6 +1,7 @@
 package Configuration;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Insets;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -12,15 +13,18 @@ public class EntityRow extends JPanel {
     JLabel label;
     JTextField textField;
     private final int FIELD_COLUMNS = 15;
-    private static final int IMAGE_SIZE = 50;
-    private static final int BUTTON_SIZE = 30;
     private static final Color BG_COLOR = new Color(245, 245, 245);
-    private static final Insets INSETS = new Insets(5, 5, 5, 5);
     
     public EntityRow(String aspect) {
         
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        setBackground(BG_COLOR);
+        
         createLabel(aspect);
         createTextField();
+        
+        add(label);
+        add(textField);
         
     }
     
@@ -34,11 +38,8 @@ public class EntityRow extends JPanel {
     }
     
     public void createLabel(String aspect) {
-        
         label = new JLabel(aspect);
-        JLabel label = new JLabel("Image");
-        label.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
-        label.setPreferredSize(new java.awt.Dimension(IMAGE_SIZE, IMAGE_SIZE));
+        label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     }
     
 }
