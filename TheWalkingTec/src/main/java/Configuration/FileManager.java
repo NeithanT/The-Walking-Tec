e99b062577dac80e58eadc0c3cfaed47eb044e83
MechaBort <f.hernandez.3@estudiantec.cr>
2025-10-25 01:56:28 -0600

    Mini Correction and cleaning
    Exchange the values cuz they were reversed xD. Also erased the netbeans useless lines

--- TheWalkingTec/src/main/java/Configuration/FileManager.java
+++ TheWalkingTec/src/main/java/Configuration/FileManager.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Configuration;
 
 
@@ -29,10 +25,7 @@ import java.util.logging.Logger;
 
 
 
-/**
- *
- * @author sap1
- */
+
 public class FileManager {
     
     private final URL PATH = getClass().getResource("/Saves");
